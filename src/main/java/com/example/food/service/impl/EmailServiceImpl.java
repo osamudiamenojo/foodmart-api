5f0eaf867f8fa6efb6ba3034ef9b5e580abbec19
engrveju <engrveju@gmail.com>
2022-12-14 15:52:12 +0100

    Added Test for emailServiceImpl and removed unused folder and classes

--- src/main/java/com/example/food/service/impl/EmailServiceImpl.java
+++ src/main/java/com/example/food/service/impl/EmailServiceImpl.java
@@ -2,44 +2,43 @@ package com.example.food.service.impl;
 
 import com.example.food.dto.EmailSenderDto;
 import com.example.food.service.EmailService;
-import com.example.food.utils.ApiResponse;
-import com.example.food.utils.ResponseProvider;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
-
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import java.util.Objects;
 
 @Component
 @AllArgsConstructor
 public class EmailServiceImpl implements EmailService {
-
     private JavaMailSender emailSender;
-    private ResponseProvider response;
     private final static Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
     @Override
-    public void sendMail(EmailSenderDto emailSenderDto) {
+    public void sendMail(EmailSenderDto emailSenderDto){
         if (
                 (Objects.nonNull(emailSenderDto.getTo())) &&
                 (Objects.nonNull(emailSenderDto.getSubject())) &&
-                 (Objects.nonNull(emailSenderDto.getContent()))
+                (Objects.nonNull(emailSenderDto.getContent()))
         ) {
+            MimeMessage mimeMessage = emailSender.createMimeMessage();
+            MimeMessageHelper message = new MimeMessageHelper(mimeMessage, "utf-8");
+            try {
+                LOGGER.info("Transferring Data from EmailSenderDto to MimeMessage helper");
 
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setTo(emailSenderDto.getTo());
-            message.setSubject(emailSenderDto.getSubject());
-            message.setText(emailSenderDto.getContent());
-            emailSender.send(message);
+                message.setTo(emailSenderDto.getTo());
+                message.setSubject(emailSenderDto.getSubject());
+                message.setText(emailSenderDto.getContent(),true);
 
-            LOGGER.info("Mail has been sent");
+                emailSender.send(mimeMessage);
 
+            } catch (MessagingException e) {
+                throw new RuntimeException(e);
+            }
+                LOGGER.info("Mail has been sent");
         }
-            LOGGER.error("Failed to send mail. Check the details you supplied " + String.valueOf(emailSenderDto));
     }
-
 }
