0b06e493e4cc4599556fa53370c51038f6626311
badmus360 <88261815+badmus360@users.noreply.github.com>
2022-12-17 12:09:20 +0100

    Forgot password implementation

---
+++ src/main/java/com/example/food/model/PasswordResetTokenEntity.java
@@ -0,0 +1,25 @@
+package com.example.food.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Table(name = "password_reset_tokens")
+@Entity
+@Setter
+@Getter
+public class PasswordResetTokenEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String token;
+
+    @OneToOne
+    @JoinColumn(name = "users_details_users_id")
+    private Users usersDetails;
+
+}
