29454d50c0e24e03a0d7cbd10d16a9c1039dd81a
badmus360 <88261815+badmus360@users.noreply.github.com>
2022-12-18 22:45:52 +0100

    Forgot password implementation

--- src/main/java/com/example/food/controllers/UserController.java
+++ src/main/java/com/example/food/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.example.food.controllers;
 import com.example.food.dto.EditUserDto;
 import com.example.food.restartifacts.BaseResponse;
 import com.example.food.services.UserService;
+import com.example.food.services.serviceImpl.UserServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,7 +15,7 @@ import javax.validation.Valid;
 @AllArgsConstructor
 public class UserController {
 
-    private final UserService userService;
+    private final UserServiceImpl userService;
 
     @PutMapping("/edit-user")
     public BaseResponse editUserDetails(@Valid @RequestBody EditUserDto editUserDto) {
--- src/main/java/com/example/food/repositories/PasswordResetTokenRepository.java
+++ src/main/java/com/example/food/repositories/PasswordResetTokenRepository.java
@@ -3,7 +3,9 @@ package com.example.food.repositories;
 import com.example.food.model.PasswordResetTokenEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PasswordResetTokenRepository extends CrudRepository<PasswordResetTokenEntity, Long> {
 
     PasswordResetTokenEntity findByToken(String token);
