360698f605efba3e74947a57596d468747f800dd
Kefas Gisung <gisungkefas@gmail.com>
2023-01-12 00:28:56 +0100

    Implemented Test for Admin to fetch all products

--- src/test/java/com/example/food/services/serviceImpl/ProductServiceImplTest.java
+++ src/test/java/com/example/food/services/serviceImpl/ProductServiceImplTest.java
@@ -1,10 +1,13 @@
 package com.example.food.services.serviceImpl;
 
 import com.example.food.Enum.ResponseCodeEnum;
+import com.example.food.dto.ProductDto;
 import com.example.food.dto.ProductSearchDto;
 import com.example.food.model.Product;
 import com.example.food.pojos.PaginatedProductResponse;
+import com.example.food.pojos.ProductResponse;
 import com.example.food.repositories.ProductRepository;
+import com.example.food.services.ProductService;
 import com.example.food.util.ResponseCodeUtil;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,11 +17,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import static org.junit.jupiter.api.Assertions.assertSame;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
@@ -31,6 +33,8 @@ class ProductServiceImplTest {
     private ProductServiceImpl productServiceImpl;
     @Mock
     private ResponseCodeUtil responseCodeUtil;
+    @Mock
+    private ProductService productService;
     @Test
     void testSearchProduct() {
         // Setting up test data
@@ -113,4 +117,51 @@ class ProductServiceImplTest {
         verify(productRepository).findAll((Pageable) any());
         verify(responseCodeUtil).updateResponseData((PaginatedProductResponse) any(), (ResponseCodeEnum) any());
     }
+
+    @Test
+    public void testFetchAllProducts_whenProductsExist() {
+
+        List<Product> products = createProductList();
+        when(productRepository.findAll()).thenReturn(products);
+
+        ProductResponse response = productService.fetchAllProducts();
+
+        assertNotNull(response, "Response object is null");
+
+        assertTrue(response.isStatus());
+        assertEquals("Products fetched successfully", response.getMessage());
+        List<ProductDto> productDto = response.getProducts();
+        assertEquals(2, productDto.size());
+        assertEquals("product1-image.jpg", productDto.get(0).getImageUrl());
+        assertEquals("Product 1", productDto.get(0).getProductName());
+
+        assertEquals("product2-image.jpg", productDto.get(1).getImageUrl());
+        assertEquals("Product 2", productDto.get(1).getProductName());
+    }
+
+    @Test
+    public void testFetchAllProducts_whenNoProductsExist() {
+
+        when(productRepository.findAll()).thenReturn(Collections.emptyList());
+
+        ProductResponse response = productService.fetchAllProducts();
+
+        assertFalse(response.isStatus());
+        assertEquals("No products found", response.getMessage());
+        assertTrue(response.getProducts().isEmpty());
+    }
+
+    private List<Product> createProductList() {
+
+        Product product1 = new Product();
+        product1.setImageUrl("product1-image.jpg");
+        product1.setProductName("Product 1");
+
+        Product product2 = new Product();
+        product2.setImageUrl("product2-image.jpg");
+        product2.setProductName("Product 2");
+
+        return Arrays.asList(product1, product2);
+    }
+
 }
