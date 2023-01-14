95c2d1e6cb45a864c1dd3e2a05c9f7112ecfa697
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-01-14 15:44:05 +0100

    FMT-23: Refactor CartRepository

--- src/main/java/com/example/food/repositories/CartRepository.java
+++ src/main/java/com/example/food/repositories/CartRepository.java
@@ -1,12 +1,10 @@
 package com.example.food.repositories;
 
 import com.example.food.model.Cart;
-import com.example.food.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
-    Cart findByUsers(Users users);
 
 }
