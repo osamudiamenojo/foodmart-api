fc3137e0bc1bb784e279be1a4b5811960a386de0
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-01-14 16:06:50 +0100

    FMT-23: Resolve Conflict in ProductRepository

--- src/main/java/com/example/food/repositories/ProductRepository.java
+++ src/main/java/com/example/food/repositories/ProductRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    Page<Product> findByProductNameContainingIgnoreCase(String filter, Pageable pageRequest);
+public interface ProductRepository extends JpaRepository<Product,Long> {
+    Page<Product> findByProductNameContainingIgnoreCase(String filter, Pageable pageable);
 }
