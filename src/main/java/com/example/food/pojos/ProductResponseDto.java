7b8b593d68845937e86320ed2c4f8ce9c3fffdf2
phoenixsylvia <sylviananev@gmail.com>
2023-01-17 15:52:35 +0100

    Fixed the service implementation logic

--- src/main/java/com/example/food/pojos/ProductResponseDto.java
+++ src/main/java/com/example/food/pojos/ProductResponseDto.java
@@ -2,6 +2,7 @@ package com.example.food.pojos;
 
 import com.example.food.dto.ProductDto;
 import com.example.food.restartifacts.BaseResponse;
+import com.example.food.services.ProductService;
 import lombok.*;
 
 @Getter
@@ -11,4 +12,5 @@ import lombok.*;
 @AllArgsConstructor
 public class ProductResponseDto extends BaseResponse {
     private ProductDto productDto;
+    private ProductService productService;
 }
