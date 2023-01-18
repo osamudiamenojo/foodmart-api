959bf56cb35b35d60b7f90c7bf1f5d5b97e9f51b
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-01-18 09:50:45 +0100

    FMT-38 Endpoint to check wallet balance Resolve conflict

--- src/main/java/com/example/food/model/Wallet.java
+++ src/main/java/com/example/food/model/Wallet.java
@@ -1,6 +1,13 @@
 package com.example.food.model;
 
-import lombok.*;
+import jdk.jfr.Timestamp;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Builder;
 
 import javax.persistence.*;
 import java.util.Date;
