3fd2d5062653cc9830650f9cdd64fb3986504a81
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-01-22 14:17:53 +0100

    FMT-20 Endpoint to add to cart

--- src/test/java/com/example/food/service/serviceImpl/CartServiceImplTest.java
+++ src/test/java/com/example/food/service/serviceImpl/CartServiceImplTest.java
@@ -102,19 +102,4 @@ public class CartServiceImplTest {
         Assertions.assertThat(cart).isNotNull();
     }
 
-    @Test
-    public void testViewCartItems() {
-        List<Cart> cartList = List.of(new Cart(1L, 20, new BigDecimal(2000), new ArrayList<>(), user));
-
-        when(userRepository.findByEmail(any())).thenReturn(Optional.of(user));
-        when(cartRepository.findAllByUsersOrderByCartId(user)).thenReturn(cartList);
-
-        CartResponse cartResponse = cartServiceImpl.viewCartItems();
-
-        Mockito.verify(cartRepository, times(1))
-                .findAllByUsersOrderByCartId(any(Users.class));
-
-        assertNotNull(cartResponse);
-        assertEquals(1, cartResponse.getCartList().size());
-    }
 }
