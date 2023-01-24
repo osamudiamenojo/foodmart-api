7723f0a4cfc77593b8647b08ed2037fbdbfe6048
bob160 <efioesien@gmail.com>
2023-01-24 20:57:34 +0100

    created endpoint to view all orders

--- src/main/java/com/example/food/repositories/OrderRepository.java
+++ src/main/java/com/example/food/repositories/OrderRepository.java
@@ -2,6 +2,11 @@ package com.example.food.repositories;
 
 import com.example.food.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface OrderRepository extends JpaRepository<Order,Long> {
+    List<Order> findAllByUser_Email(String email);
 }
