7c79bf068f7317a3d1ac03760894b4db57826abd
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-01-24 23:02:32 +0100

    Test to Endpoint to Clear Cart

--- src/main/java/com/example/food/controllers/CartController.java
+++ src/main/java/com/example/food/controllers/CartController.java
@@ -1,18 +1,12 @@
 package com.example.food.controllers;
 
-import com.example.food.dto.CartDto;
-import com.example.food.dto.EditUserDto;
-import com.example.food.model.Users;
 import com.example.food.pojos.CartResponse;
 import com.example.food.restartifacts.BaseResponse;
 import com.example.food.services.CartService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
 
 @AllArgsConstructor
 @RestController
@@ -36,7 +30,7 @@ public class CartController {
     @DeleteMapping("/clear-cart")
     public ResponseEntity<CartResponse> clearCart() {
         CartResponse response = cartService.clearCart();
-        return new ResponseEntity<>(response, HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 }
